05cdb50a21d9682f14764944a8cff5ca7f3dbb05
Alexander Schneider <alexander@schneider.gg>
2018-08-09 18:42:22 +0200

    Add basic elo rating function

--- src/rating/elo.clj
+++ src/rating/elo.clj
@@ -1,7 +1,27 @@
 (ns rating.elo)
 
+(defn- transact-player
+  ([rating expected won]
+   (transact-player rating expected won 10))
+  ([rating expected won k-value]
+   (Math/round (+ rating
+                  (* 10
+                     (- won expected))))))
+
 (defn rate
-  "Expects the current ratings of two players and an indicator who won and returns the new ratings of both players in order."
-  [rating-one rating-two winner]
-  [rating-one rating-two] ;; stub
-  )
+  "Expects the current ratings of two players and an indicator who won and returns the new ratings of both players in order. This uses a default k value of 10 if none is provided."
+
+  ([rating-one rating-two winner]
+   (rate rating-one rating-two winner 10))
+  
+  ([rating-one rating-two winner k-value]
+   (let [expected-one (/ 1
+                         (inc (Math/pow 10
+                                        (/ (- rating-two rating-one) 400))))]
+     (case winner
+       1 [(transact-player rating-one expected-one 1 k-value)
+          (transact-player rating-two (- 1 expected-one) 0 k-value)]
+       2 [(transact-player rating-one expected-one 0 k-value)
+          (transact-player rating-two (- 1 expected-one) 1 k-value)]
+       0 [(transact-player rating-one expected-one 0.5 k-value)
+          (transact-player rating-two (- 1 expected-one) 0.5 k-value)]))))
